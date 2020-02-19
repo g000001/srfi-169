f907e254f8302f4f88fc5bd21b4b367638cc90cf
g000001 <chiba.masaomi@gmail.com>
2020-02-20 04:58:40 +0900

    fix +-reader

--- readtable.lisp
+++ readtable.lisp
@@ -3,9 +3,10 @@
 
 
 (cl:eval-when (:compile-toplevel :load-toplevel :execute)
-  (cl:defvar *srfi-169-syntax* (cl:copy-readtable () ))
+  (cl:defvar *srfi-169-syntax* (cl:copy-readtable #f))
 
-  (let ((cl:*readtable* *srfi-169-syntax*))
+  (let ((cl:*readtable* *srfi-169-syntax*)
+        (stdrt (cl:copy-readtable #f)))
     (cl:flet ((setmc (c)
                 (cl:set-macro-character
                  c
@@ -13,6 +14,20 @@
                    (cl:unread-char chr stm)
                    (let ((cl:*standard-input* stm))
                      (read-number)))
+                 #t))
+              (set+- (c)
+                (cl:set-macro-character
+                 c
+                 (lambda (stm chr)
+                   (let ((cl:*standard-input* stm))
+                     (cond ((cl:find (peek-char) "0123456789")
+                            (if (cl:eql #\- chr)
+                                (cl:- (read-number))
+                                (read-number)))
+                           (else
+                             (cl:unread-char chr stm)
+                             (let ((cl:*readtable* stdrt))
+                               (read))))))
                  #t))
               (setdmc (c)
                 (cl:set-dispatch-macro-character
@@ -23,7 +38,8 @@
                    (cl:unread-char #\# stm)
                    (let ((cl:*standard-input* stm))
                      (read-number))))))
-      (cl:map #f #'setmc "+-0123456789")
+      (cl:map #f #'setmc "0123456789")
+      (cl:map #f #'set+- "+-")
       (cl:map #f #'setdmc "bodx")))
   (cl:defconstant srfi-169-syntax *srfi-169-syntax*))
 
